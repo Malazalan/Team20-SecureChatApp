aa96fd64e1b4b3cf105a2e1d9559ca49d2f1066f
ditnam <ditnam.midd@gmail.com>
2024-03-08 17:43:54 +0000

    Initial commit The header will store the messages type (regular text or file) and the number of packets to be transmitted / received

---
+++ Client/Header.py
@@ -0,0 +1,22 @@
+from enum import Enum
+
+
+HEADER_LENGTH = 16
+
+class Message_Type(Enum):
+    TEXT = 1
+    FILE = 2
+
+
+class Header:
+    def __init__(self, message_type, number_of_packets):
+        self.type = message_type
+        self.number_of_packets = number_of_packets
+
+    def get_header_bytes(self):
+        byte_header = (self.type).value.to_bytes(1, byteorder='little') + self.number_of_packets.to_bytes((HEADER_LENGTH - 1), byteorder='little')
+        return byte_header
+
+
+test = Header(Message_Type.TEXT, 3)
+print(f"{test.get_header_bytes()}\n{len(test.get_header_bytes())}")
