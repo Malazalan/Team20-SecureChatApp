2eb624f89ab0d3b6fc2050fbb6c01ad375cfeeee
ditnam <ditnam.midd@gmail.com>
2024-03-09 17:46:56 +0000

    Messages are now forward to another (currently all go to localhost)

--- Server/server.c
+++ Server/server.c
@@ -6,10 +6,12 @@
 #include <netinet/in.h>
 #include <pthread.h>
 #include <string.h>
+#include <arpa/inet.h>
 
 #define PORT 54321
+#define CLIENT_PORT 12345
 #define MAX_CONN 5
-#define MAX_PACKET_LENGTH 128
+#define MAX_PACKET_LENGTH 12
 #define HEADER_LENGTH 16
 
 // Header types
@@ -17,6 +19,92 @@
 #define file 2
 
 pthread_mutex_t lock;
+
+struct Header {
+    int messageType;
+    int numberOfPackets;
+};
+
+void convertHeaderToBytes(struct Header header, unsigned char* headerBytes) {
+    headerBytes[0] = header.messageType;
+
+    for (int i = 1; i < HEADER_LENGTH; i++) {
+        headerBytes[i] = header.numberOfPackets & 0xFF; // Extract the least significant byte
+        header.numberOfPackets >>= 8; // Shift right by 8 bits to get the next byte
+    }
+    for (int i = 0; i < HEADER_LENGTH; i++) {
+        printf("%02X ", headerBytes[i]);
+    }
+}
+
+void handleWrite(char* address, struct Header * header, const unsigned char* messageToSend) {
+    int sockfd;
+    struct sockaddr_in clientAddr;
+
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) {
+        printf("Failed to create write socket");
+        pthread_exit(NULL);
+    }
+
+    clientAddr.sin_family = AF_INET;
+    clientAddr.sin_addr.s_addr = inet_addr(address);
+    clientAddr.sin_port = htons(CLIENT_PORT);
+
+    if (connect(sockfd, (struct sockaddr*)&clientAddr, sizeof(clientAddr)) != 0) {
+        printf("Failed to connect to client listener");
+        pthread_exit(NULL);
+    }
+
+    printf("Size %ld\n", strlen(messageToSend));
+    unsigned char headerBytes[HEADER_LENGTH];
+    convertHeaderToBytes(*header, headerBytes);
+
+    // Send the header
+    printf("Sending header\n");
+    send(sockfd, headerBytes, HEADER_LENGTH, 0);
+
+    // Send the packets
+    printf("Sending message\n");
+    printf("Preparing %s\n", messageToSend);
+    for (int packetNum = 0; packetNum < header->numberOfPackets; packetNum++) {
+        unsigned char* buffer = malloc(MAX_PACKET_LENGTH);
+
+        if (packetNum < header->numberOfPackets - 1) {
+            for (int i = 0; i < MAX_PACKET_LENGTH; i++) {
+                buffer[i] = messageToSend[(packetNum * MAX_PACKET_LENGTH) + i];
+            }
+        } else {
+            int counter = 0;
+            for (int i = 0; i < MAX_PACKET_LENGTH; i++) {
+                if ((packetNum * MAX_PACKET_LENGTH) + i < strlen((const char*)messageToSend)) {
+                    buffer[i] = messageToSend[(packetNum * MAX_PACKET_LENGTH) + i];
+                    counter++;
+                } else {
+                    for (int j = counter; j < MAX_PACKET_LENGTH; j++) {
+                        buffer[j] = '\0';
+                    }
+                }
+            }
+        }
+
+        printf("Sent %s\n", buffer);
+        send(sockfd, buffer, MAX_PACKET_LENGTH, 0);
+        free(buffer);
+    }
+    printf("Finished\n\n");
+}
+
+struct WriteThreadArgs {
+    char * addr;
+    struct Header * header;
+    unsigned char messageToSend;
+};
+
+void *writeThreadWrapper(void* args) {
+    struct WriteThreadArgs* threadArgs = (struct WriteThreadArgs*) args;
+
+}
 
 char** splitMessage(char* receivedMessage, int* tokenCount) {
     const char delim[2] = "\x1F";
@@ -91,6 +179,7 @@ int main(int argc, char * argv[]) {
             exit(EXIT_FAILURE);
         }
 
+        printf("Ready to receive\n");
         if ((new_client = accept(server_fd, (struct sockaddr*) &address, &addrlen)) < 0) {
             perror("Error accepting new client connection");
             exit(EXIT_FAILURE);
@@ -135,12 +224,31 @@ int main(int argc, char * argv[]) {
         printf("Target - %s\n", tokens[3]);
         printf("Timestamp - %s\n", tokens[4]);
 
+        struct Header headerToSend;
+        headerToSend.numberOfPackets = num_packets;
+        headerToSend.messageType = messageType;
+
+        printf("\n\n");
+
+        int messageLen = 0;
+        for(int i = 0; i < tokenCount; i++) {
+            messageLen += strlen(tokens[i]);
+        }
+
+        printf("%d\n", messageLen);
+        char messageToSend[messageLen];
+        for (int i = 0; i < tokenCount; i++) {
+            strcat(messageToSend, tokens[i]);
+        }
+
+        printf("%s\n%s\n---\n", messageToSend, receivedMessage);
+
+        handleWrite(Alice, &headerToSend, (unsigned char*)messageToSend);
+
         for(int i = 0; i < tokenCount; i++) {
             free(tokens[i]);
         }
         free(tokens);
-
-        printf("\n\n");
     }
     // Cleanup
     close(server_fd);
