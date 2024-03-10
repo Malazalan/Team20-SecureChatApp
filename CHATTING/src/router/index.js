3a57dd0511b07f5ce22a333f25a75f64847f255a
Emiily <C3036807@newcastle.ac.uk>
2024-03-11 02:43:11 +0800

    UI

---
+++ CHATTING/src/router/index.js
@@ -0,0 +1,25 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: '/login',
+    name: 'Login',
+    component: () => import('@/views/Login.vue')
+  },
+  {
+    path: '/',
+    component: () => import('@/views/Home/index.vue'),
+    meta: { title: 'Home' }
+  }
+];
+
+const router = new VueRouter({
+  mode: 'hash',
+  base: process.env.BASE_URL,
+  routes
+});
+
+export default router;
