1b5a20fda5daaa992e36940c05954300bcc1f8ed
ditnam <ditnam.midd@gmail.com>
2024-03-13 18:00:41 +0000

    The app now quits when Ctrl+C is pressed

--- Client/SocketCommsTestUI.py
+++ Client/SocketCommsTestUI.py
@@ -1,5 +1,7 @@
-import os
 from SocketComms import *
+from EncryptionService import *
+
+import os
 import queue
 import threading
 import signal
@@ -8,16 +10,13 @@ input_queue = queue.Queue()
 
 sender, target = "", ""
 
+aes_key = os.urandom(32)
 
-def signal_handler(sig, frame):
-    global keepRunning
-    with stdout_mutex:
-        print("Ctrl+C received, killing threads...")
-    keepRunning = False
+
 
 
 def read_input(input_queue):
-    while keepRunning:
+    while get_keep_running():
         try:
             # Python 2 compatibility: use raw_input() instead of input()
             inp = input()
@@ -42,16 +41,14 @@ def redraw_UI():
     print(f"\n\n\n{sender}:    ")
 
 
-
 if __name__ == "__main__":
-    keepRunning = True
     if len(sys.argv) != 4:  # Remember, the first arg is the script name itself
         print("Usage: python3 SocketCommsTestUI.py Your username | Their username | Server IP")
         sys.exit(1)
 
     _, sender, target, server_ip = sys.argv  # Unpack the arguments
 
-    listen_thread = threading.Thread(target=server_listen_handler)
+    listen_thread = threading.Thread(target=server_listen_handler, args=(get_private_key(),))
     listen_thread.start()
 
     input_thread = threading.Thread(target=read_input, args=(input_queue,))
@@ -62,13 +59,14 @@ if __name__ == "__main__":
 
     num_messages = len(messages)
     redraw_UI()
-    while keepRunning:
+    while get_keep_running():
         if len(messages) > num_messages:
             num_messages = len(messages)
             redraw_UI()
         try:
             user_input = input_queue.get_nowait()
-            writeThread = threading.Thread(target=prepare_message, args=(sender, target, user_input, "", "", Message_Type.TEXT))
+            writeThread = threading.Thread(target=prepare_message,
+                                           args=(sender, target, user_input, get_public_key(), get_server_public_key(), Message_Type.TEXT, server_ip))
             writeThread.start()
             messages.append([user_input, "", sender])
         except queue.Empty:
@@ -76,4 +74,8 @@ if __name__ == "__main__":
 
         time.sleep(0.1)
 
-    listen_thread.join()
+    listen_thread.join(timeout=5)  # Timeout in seconds
+    if listen_thread.is_alive():
+        print("Warning: listen_thread did not terminate gracefully.")
+    else:
+        print("listen_thread terminated gracefully")
