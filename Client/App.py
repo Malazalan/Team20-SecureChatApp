3f4a109ad94fc1b74f8a63085fe01caac34de7d8
joeb1603 <joeb1603@gmail.com>
2024-03-15 15:58:14 +0000

    Added support for sending files (of any type)

--- Client/App.py
+++ Client/App.py
@@ -3,7 +3,7 @@ from flask import render_template, request, redirect, url_for
 from flask_login import LoginManager, login_user, login_required, current_user, logout_user
 from flask_socketio import SocketIO
 import importlib
-
+import os
 import Database
 importlib.reload(Database)
 import User
@@ -11,20 +11,24 @@ importlib.reload(User)
 import SocketComms
 importlib.reload(SocketComms)
 
-import threading
-
 from User import User
 from Database import get_user, get_user_ids
 from SocketComms import * # should maybe be changed but * works for now
 
 app = Flask(__name__)
-socketio = SocketIO(app)
+socketio = SocketIO(app, max_http_buffer_size=10000000)
 
 app.secret_key = "very_secret_key" # TODO: this
 
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login_page'
+
+
+if os.path.exists("cert.pem") and os.path.exists("key.pem"):
+    ip_addr = "https://team20.joe-bainbridge.com/"
+else:
+    ip_addr = "http://127.0.0.1:5000"
 
 @app.route('/')
 def login_page():
@@ -43,7 +47,7 @@ def home_page():
 def chat_page(target_user_id):
     target_user = get_user(target_user_id)
     if target_user is not None:
-        return render_template('chat.html', target_user_id = target_user_id)
+        return render_template('chat.html', target_user_id = target_user_id, ip_addr=ip_addr)
     return redirect(url_for('home_page'))
 
 @app.route('/login', methods = ['GET', 'POST'])
@@ -55,11 +59,11 @@ def login_function():
         if username and password:
             user = get_user(username)
             if user is not None:
-                if user.validate_fingerprint(browser_fingerprint): #TODO: failure message
+                if user.validate_fingerprint(browser_fingerprint): 
                     if user.password_correct(password):
                         login_user(user)
                         return redirect(url_for('home_page'))
-    return redirect(url_for('login_page')) # TODO: add failure message?
+    return redirect(url_for('login_page')) # TODO: add failure message and keep username in the box
 
 @app.route('/register/<target_user>/<invite_id>')
 def register_page(target_user, invite_id):
@@ -68,7 +72,7 @@ def register_page(target_user, invite_id):
     invite = Database.get_invite(target_user)
     if invite and invite['invite_id']==invite_id and not get_user(invite['_id']):
         return render_template('register.html', username = target_user)
-    return redirect(url_for('login_page')) #TODO: need error handling telling the user it was an invalid invite?
+    return redirect(url_for('login_page')) 
 
 @app.route('/register_submit', methods = ['GET', 'POST'])
 def register_function():
@@ -77,7 +81,7 @@ def register_function():
 
         browser_fingerprint = request.form.get('browserFingerprint') 
         username = request.headers.get('Referer').split('/')[-2]          
-        email = "bleh@gmail.com"                                         
+        email = "bleh@gmail.com" #TODO: remove all refrences to email in all the files - it is not used anymore                                      
         
         user = get_user(username)
         if user is None:  
@@ -102,30 +106,15 @@ def load_user(username):
 @socketio.on('user_connect')
 def handle_join_room_event(data):
     app.logger.info(f"{data['username']} has opened a chat with {data['target']} ")
-    #print(Database.get_room(current_user.get_id()))
-    Database.set_room(current_user.get_id(), request.sid)
-    #print(f"REQUEST: {request.sid}")
+    Database.set_room(current_user.get_id(), request.sid) # For extra privacy, could this be stored in the servers memory?
 
 @socketio.on('disconnect')
 def handle_disconnect_event():
-    # Handle disconnection logic here
-    Database.set_room(current_user.get_id(), None)
-    #print(f"User disconnected {request.sid}")
-
-
+    Database.set_room(current_user.get_id(), None) # For extra privacy, could this be stored in the servers memory?
+   
 @socketio.on('message_sent')
 def handle_message_sent(data):
-    app.logger.info(f"{data['username']} has sent {data['message']} to {data['target']}")
-    #print(f"REQUEST: {request.sid}")
-    '''try:
-        #SocketComms.py stuff
-        #the line below is here just as a test to see if the lines below that will fail the try 
-        prepare_message("Alice", "Bob", f"{data['message']}", "", "")
-        writeThread = threading.Thread(target=prepare_message, args=("Alice", "Bob", f"{data['message']}", "", ""))
-        writeThread.start()
-    except:
-        print("Server stuff not working")'''
-
+    app.logger.info(f"{data['username']} has sent {data['message']} to {data['target']}") #this is temporary
     target = get_user(data['target'])
     if target:
         sender_room = request.sid
@@ -138,9 +127,42 @@ def handle_message_sent(data):
             failure_data['message'] = "Message failed to send, target user not online"
             socketio.emit('recieve_message', data, room=sender_room)
            
-            
-    
+@socketio.on('file_sent')
+def handle_file_sent(data):
+    file = data['suitable_name'] # would not work just being called 'file', so i figured this was the next best name
+    file_name = data['file_name']
 
+    if True: # TODO: Input checking on file maybe?
+        print("2")
+        target = get_user(data['target'])
+        if target:
+            sender_room = request.sid
+            target_room = target.current_room
+            if target_room is not None:
+                socketio.emit('recieve_file', {
+                    'username': data['username'],
+                    'target': data['target'],
+                    'file': file,
+                    'file_name': file_name,
+
+                }, room=sender_room)
+                socketio.emit('recieve_file', {
+                    'username': data['username'],
+                    'target': data['target'],
+                    'file': file,
+                    'file_name': file_name,
+
+                }, room=target_room)              
+            else:
+                failure_data = data
+                failure_data['message'] = "File failed to send, target user not online"
+                socketio.emit('recieve_message', data, room=sender_room)
+        
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
+    if os.path.exists("cert.pem") and os.path.exists("key.pem"):
+        socketio.run(app, debug=True, ssl_context=('cert.pem', 'key.pem'), port=443)
+    else:
+        socketio.run(app, debug=True)
+
+        
 
