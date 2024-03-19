43e081eec97bc10aac2d96f6b555b30ee51e032f
ditnam <ditnam.midd@gmail.com>
2024-03-19 14:13:40 +0000

    Received messaeges are now sent as a socketio emission from the flask server to the client instance

--- Client/SocketComms.py
+++ Client/SocketComms.py
@@ -7,9 +7,11 @@ import signal
 import sys
 import time
 
-from EncryptionService import rsa_decrypt, get_private_key, get_public_key, sign, verify_signature
+import socketio
+from EncryptionService import   rsa_decrypt, get_private_key, get_public_key, sign, verify_signature
 from Message import *
 from Header import *
+from Database import get_public_key_from_user, get_user
 
 MAX_PACKET_LENGTH = 256
 MAX_RECONN = 15
@@ -44,7 +46,7 @@ def signal_handler(sig, frame):
     keepRunning = False
 
 
-def server_listen_handler(private_key):
+def server_listen_handler(private_key, socketio):
     global keepRunning
 
     print("Starting listener")
@@ -84,12 +86,25 @@ def server_listen_handler(private_key):
                 #print(type(private_key))
                 #print(type(message))
                 #print(type(message_header.type))
-                temp = decrypt_wrapper(private_key, message, message_header.type)
-                split_message = decrypt_wrapper(private_key, message, message_header.type).decode('utf-8').split(chr(30))
+                temp = decrypt_wrapper(message, message_header.type)
+                split_message = decrypt_wrapper(message, message_header.type).decode('utf-8').split(chr(30))
                 # TODO actually get the right public key
                 bytes_signature = ast.literal_eval(split_message[1])
-                split_message[2] += f" - {verify_signature(get_public_key(), split_message[0].encode('utf-8'), bytes_signature)}"
-                messages.append(split_message)
+                split_message[2] += f" - {verify_signature(get_public_key_from_user(split_message[2]), split_message[0].encode('utf-8'), bytes_signature)}"
+
+                data = {
+                    "username": split_message[2],
+                    "target": split_message[4],
+                    "message": split_message[0],
+                    "is_encrypted": False
+                }
+
+                target = get_user(split_message[4])
+                print(target)
+                target_room = target.current_room
+                socketio.emit('receive_message', data, room=target_room)
+                print("Yay??")
+
             elif message_header.type == Message_Type.FILE.value:
                 #print("Received contents")
                 #print(decrypt_wrapper(private_key, message, message_header.type))
@@ -173,14 +188,13 @@ def server_send_handler(message_to_send, metadata_to_send, message_type, server_
                         attempts += 1
 
 
-def prepare_message(sender, target, data, target_public_key, server_public_key, message_type, server_ips):
+def prepare_message(sender, target, data, target_public_key, message_type, server_ips):
     if message_type == Message_Type.TEXT:
         #print(f"\tData 1 - {data.encode('utf-8')}")
         message_signature = sign(data.encode('utf-8'))
         message_to_send = Message(data, target, sender, message_signature)
 
-        message, metadata = form_message_blocks(message_to_send, MAX_PACKET_LENGTH, target_public_key,
-                                                server_public_key)
+        message, metadata = form_message_blocks(message_to_send, MAX_PACKET_LENGTH, target_public_key)
     elif message_type == Message_Type.FILE:
         with open(data, mode="rb") as file:
             file_bytes = file.read()
@@ -191,8 +205,7 @@ def prepare_message(sender, target, data, target_public_key, server_public_key, 
         #print(message_signature)
         message_to_send = MessageFile(data, target, sender, message_signature)
 
-        message, metadata = form_message_blocks(message_to_send, MAX_PACKET_LENGTH, target_public_key,
-                                                server_public_key)
+        message, metadata = form_message_blocks(message_to_send, MAX_PACKET_LENGTH, target_public_key)
     else:
         print(f"Invalid type - {message_type}")
         return
